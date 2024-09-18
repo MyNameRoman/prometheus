c8a7c2cbfd9c3cfba1d15914ce1caeac2c1e92ce
MyNameRoman <157910852+MyNameRoman@users.noreply.github.com>
2024-09-18 18:46:48 +0300

    Update install_prometheus_server_ubuntu.sh

--- install_prometheus_server_ubuntu.sh
+++ install_prometheus_server_ubuntu.sh
@@ -4,46 +4,59 @@
 # Tested on Ubuntu 22.04, 24.04
 # Developed by Denis Astahov in 2024
 #--------------------------------------------------------------------
-PROMETHEUS_VERSION="2.51.1"
+
+# Указываем версию Prometheus
+PROMETHEUS_VERSION="2.46.0"
+
 PROMETHEUS_FOLDER_CONFIG="/etc/prometheus"
 PROMETHEUS_FOLDER_TSDATA="/etc/prometheus/data"
 
+# Загружаем Prometheus из официального репозитория
 cd /tmp
 wget https://github.com/prometheus/prometheus/releases/download/v$PROMETHEUS_VERSION/prometheus-$PROMETHEUS_VERSION.linux-amd64.tar.gz
+if [ $? -ne 0 ]; then
+  echo "Ошибка при загрузке Prometheus. Проверьте URL или доступность интернета."
+  exit 1
+fi
+
+# Распаковываем архив
 tar xvfz prometheus-$PROMETHEUS_VERSION.linux-amd64.tar.gz
 cd prometheus-$PROMETHEUS_VERSION.linux-amd64
 
+# Переносим бинарный файл в /usr/bin
 mv prometheus /usr/bin/
 rm -rf /tmp/prometheus*
 
+# Создаем необходимые каталоги
 mkdir -p $PROMETHEUS_FOLDER_CONFIG
 mkdir -p $PROMETHEUS_FOLDER_TSDATA
 
-
+# Конфигурация Prometheus
 cat <<EOF> $PROMETHEUS_FOLDER_CONFIG/prometheus.yml
 global:
   scrape_interval: 15s
 
-# Тут редачить айпишники для отслеживания серверов
 scrape_configs:
   - job_name      : "prometheus"
     static_configs:
       - targets: ["localhost:9090"]
 EOF
 
+# Создаем пользователя для Prometheus
 useradd -rs /bin/false prometheus
+
+# Устанавливаем права на файлы и директории
 chown prometheus:prometheus /usr/bin/prometheus
 chown prometheus:prometheus $PROMETHEUS_FOLDER_CONFIG
 chown prometheus:prometheus $PROMETHEUS_FOLDER_CONFIG/prometheus.yml
 chown prometheus:prometheus $PROMETHEUS_FOLDER_TSDATA
 
-
+# Создаем systemd-сервис для управления Prometheus
 cat <<EOF> /etc/systemd/system/prometheus.service
 [Unit]
 Description=Prometheus Server
 After=network.target
 
-# Конфиг прометеуса
 [Service]
 User=prometheus
 Group=prometheus
@@ -57,9 +70,13 @@ ExecStart=/usr/bin/prometheus \
 WantedBy=multi-user.target
 EOF
 
+# Перезапускаем systemd и включаем сервис Prometheus
 systemctl daemon-reload
 systemctl start prometheus
 systemctl enable prometheus
+
+# Проверка статуса Prometheus
 systemctl status prometheus --no-pager
+
+# Вывод версии Prometheus
 prometheus --version
-`echo “<3P ”`
